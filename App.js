4869d90fc91203d4b8f517319b6ee1442a8fe4b7
Oscar Franco <ospfranco@protonmail.com>
2022-08-25 21:52:44 +0200

    Add axios

--- App.js
+++ App.js
@@ -3,8 +3,10 @@ import {
   Button,
   SafeAreaView, Text
 } from 'react-native';
+import axios from 'axios'
 
 import { JsiHttp } from 'react-native-jsi-cpr';
+import performance from 'react-native-performance';
 
 const http = new JsiHttp({
   baseUrl: 'https://api.github.com',
@@ -15,37 +17,40 @@ async function sleep() {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve()
-    }, 100)
+    }, 500)
   })
 }
 
 const App = () => {
   const [fetchTime, setFetchTime] = useState(0)
   const [cprTime, setCprTime] = useState(0)
+  const [axiosTime, setAxiosTime] = useState(0)
 
   const fetchData = async () => {
-    let now = new Date()
+    let now = +new Date()
     const fetchRes = await fetch('https://api.github.com/search/repositories?q=ospfranco')
     await fetchRes.json()
-    setFetchTime(new Date().getMilliseconds() - now.getMilliseconds())
+    setFetchTime(+new Date() - now)
 
     // Wait for system to stabilize again
     await sleep()
 
-    now = new Date()
-    const jsiRes = await http.get('/search/repositories?q=ospfranco')
-    setCprTime(new Date().getMilliseconds() - now.getMilliseconds())
+    now = +new Date()
+    await http.get('/search/repositories?q=ospfranco')
+    setCprTime(+new Date() - now)
 
+    await sleep()
 
+    now = +new Date()
+    await axios.get('https://api.github.com/search/repositories?q=ospfranco')
+    setAxiosTime(+new Date() - now)
   }
 
   return (
-    <SafeAreaView style={{alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Github.com</Text>
-      <Text>Fetch request:</Text>
-      <Text>{fetchTime} ms</Text>
-      <Text>Cpr request:</Text>
-      <Text>{cprTime} ms</Text>
+    <SafeAreaView style={{alignItems: 'center', justifyContent: 'center', backgroundColor: 'white', flex: 1}}>
+      <Text>Fetch: {fetchTime} ms</Text>
+      <Text>JSI Cpr: {cprTime} ms</Text>
+      <Text>Axios: {axiosTime} ms</Text>
       <Button title='Fetch data' onPress={fetchData}/>
     </SafeAreaView>
   );
